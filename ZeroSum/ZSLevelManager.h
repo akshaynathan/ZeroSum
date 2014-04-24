2f9e7c99ec7936fd46c35859e4b76f3bb09248ad
Akshay Nathan <akshaynathan@akshays-air.wireless.yale.internal>
2014-04-24 04:16:49 -0400

    Some file moving

--- ZeroSum/ZSLevelManager.h
+++ ZeroSum/ZSLevelManager.h
@@ -2,16 +2,23 @@
 //  ZSLevelManager.h
 //  ZeroSum
 //
-//  Created by Akshay Nathan on 4/23/14.
+//  Created by Akshay Nathan on 4/24/14.
 //  Copyright (c) 2014 AkshayNathan. All rights reserved.
 //
 
 #import <SpriteKit/SpriteKit.h>
+#import "ZSBoardNode.h"
+#import "ZSNewTileNode.h"
 
 @interface ZSLevelManager : SKNode
 
--(id)init;
+// Initializes the manager for a given board
+-(ZSLevelManager*)initWithBoard:(ZSBoardNode*)n;
 
+// Start the levels
 -(void)start;
+
+// Changes the given newtile to an actual tile (deletes the newtile)
+-(void)createReal:(ZSNewTileNode*)tile;
 
 @end
--- ZeroSum/ZSLevelManager.m
+++ ZeroSum/ZSLevelManager.m
@@ -2,16 +2,28 @@
 //  ZSLevelManager.m
 //  ZeroSum
 //
-//  Created by Akshay Nathan on 4/23/14.
+//  Created by Akshay Nathan on 4/24/14.
 //  Copyright (c) 2014 AkshayNathan. All rights reserved.
 //
 
 #import "ZSLevelManager.h"
 
-@implementation ZSLevelManager
+@implementation ZSLevelManager {
+    ZSBoardNode* board;
+}
 
--(id)init {
-    
+-(ZSLevelManager*)initWithBoard:(ZSBoardNode*)n {
+    self = [super init];
+    board = n;
+    return self;
+}
+
+-(void)newTileAtColumn:(int)column {
+    [board addChild:[ZSNewTileNode nodeWithValue:3 inColumn:4 waitingFor:10.0]];
+}
+
+-(void)createReal:(ZSNewTileNode*)tile {
+    [board addChild:[ZSNewTileNode nodeWithValue:3 inColumn:4 waitingFor:10.0]];
 }
 
 -(void)start {
